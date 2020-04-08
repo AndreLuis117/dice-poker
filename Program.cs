89d8f2f1abaf5e97d0a0c894ee77c48f5a8398cb
AndreLuis117 <andreluis2006@outlook.com>
2020-04-08 20:16:29 -0300

    Estrtura inicial do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dice_poker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
